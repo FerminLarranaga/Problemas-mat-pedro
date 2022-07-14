ce2fe9e80ad8ae77a8a5eb37f528b53d5aeb4b06
Fermín Larrañaga <fermin.larranaga05@gmail.com>
2022-07-14 19:50:33 -0300

    Update practica 2-7-22.js

--- practica 2-7-22.js
+++ practica 2-7-22.js
@@ -21,7 +21,7 @@ var problemas = [
     "Los chicos de jardin quieren pasar de un lado del rio al otro lado, en el barco solo se pueden sentar 8 personas, y el profesor y los alumnos son en total 57 personas. Si el profesor tiene que ir en cada viaje del barco, cuantas veces hay que usar el barco para que todos pasen al otro lado del rio?",
     "Un colegio necesita comprar sillas y mesas para los salones. Cada mesa necesita 4 sillas, y cada salon necesita 7 mesas. Si cada mesa vale 650$ y cada silla vale cuatro veces menos que las mesas, cuanta plata tendra que gastar el colegio si hay 24 salones?",
 ]
-var resultados = [11640, 20, 1390, 425, 624, 125, 1296, 14, 85, 150, 9000, 5635, 60, 150000, 123, 22350, 23, 7, 218400]
+var resultados = [11640, 20, 1390, 425, 624, 125, 1296, 14, 85, 150, 9000, 5635, 60, 150000, 123, 22350, 23, 8, 218400]
 
 var numero1 = prompt("elige un numero del 1 al " + (problemas.length) + " o te mato")
 
