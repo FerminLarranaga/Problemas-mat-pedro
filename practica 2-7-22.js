d3efac2d717bfe40d29d8bad6b1989cb2186827c
Fermín Larrañaga <fermin.larranaga05@gmail.com>
2022-07-19 15:12:45 -0300

    Update practica 2-7-22.js

--- practica 2-7-22.js
+++ practica 2-7-22.js
@@ -19,9 +19,9 @@ var problemas = [
     "Pedro tiene que comprar 68 pares de zapatillas. La mitad de los pares tienen un precio de 450$, y la otra mitad de los pares valen la mitad de los pares anteriores. Cada 10 pares de zapatillas, hay un descuento de 100$. Cuanto gasto pedro en total?",
     "El precio orginal de la ropa en un shopping es 100$, pero ahora esta en oferta de 80% de descuento. Si vos tenes 460$ pesos, cuanta ropa podes comprar?",
     "Los chicos de jardin quieren pasar de un lado del rio al otro lado, en el barco solo se pueden sentar 8 personas, y el profesor y los alumnos son en total 57 personas. Si el profesor tiene que ir en cada viaje del barco, cuantas veces hay que usar el barco para que todos pasen al otro lado del rio?",
-    "Un colegio necesita comprar sillas y mesas para los salones. Cada mesa necesita 4 sillas, y cada salon necesita 7 mesas. Si cada mesa vale 650$ y cada silla vale cuatro veces menos que las mesas, cuanta plata tendra que gastar el colegio si hay 24 salones?",
+    "Un colegio necesita comprar sillas y mesas para los salones. Cada mesa necesita 5 sillas, y cada salon necesita 3 mesas. Si cada mesa vale 340$ y cada silla vale cuatro veces menos que las mesas, cuanta plata tendra que gastar el colegio si hay 24 salones?",
 ]
-var resultados = [11640, 20, 1390, 425, 624, 125, 1296, 14, 85, 150, 9000, 5635, 60, 150000, 123, 22350, 23, 8, 218400]
+var resultados = [11640, 20, 1390, 425, 624, 125, 1296, 14, 85, 150, 9000, 5635, 60, 150000, 123, 22350, 23, 8, 55080]
 
 var numero1 = prompt("elige un numero del 1 al " + (problemas.length) + " o te mato")
 
